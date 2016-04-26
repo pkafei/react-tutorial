185efdacd70e2d02ec37fc3dd700977574371204
Portia Burton <plburton@gmail.com>
2016-04-26 08:16:58 -0400

    Added form and comment form events

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -21,11 +21,32 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', text:''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+        return;
+    }
+    // TODO: send request to the server
+    this.setState({author: '', text: ''});
+  },
   render: function() {
     return (
-      <div className="CommentForm">
-        Hello, bitches! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your Name" value={this.state.author} onChange={this.handleAuthorChange} />
+        <input type="text" placeholder="Say something..." value={this.state.text} onChange={this.handleTextChange} />
+        <input type="submit" value="Post" />
+      </form>
       );
   }
 });
